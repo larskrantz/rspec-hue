2f0ae594258431a566d32dd389a620d48b181340
Lars Krantz <lars.krantz@alaz.se>
2013-05-28 22:48:15 +0200

    Renamed r_hue to rspec_hue and added spec

---
+++ lib/rspec_hue.rb
@@ -0,0 +1,43 @@
+require 'rspec'
+require 'philips_hue_controller'
+
+class RSpecHue 
+	def initialize output, additional_args = {}
+		@output = output || StringIO.new
+		@bulb_controller = additional_args.fetch(:controller) { setup_philips_hue_controller }
+	end
+	def dump_summary(duration, example_count, failure_count, pending_count)
+		if failure_count > 0 
+			bulb_controller.failed
+		else
+			bubl_controller.passed
+		end
+	end
+	def method_missing(m, *args, &block) end
+
+	def self.configure()
+		RSpec.configure do |c|
+			c.add_setting :rspec_hue_bulb_id
+			c.add_setting :rspec_hue_ip
+			c.add_setting :rspec_hue_failed_color
+			c.add_setting :rspec_hue_passed_color
+		end
+	end
+
+	private
+	def setup_philips_hue_controller
+		options = {bulb_id: RSpec.configuration.rspec_hue_bulb_id }
+		failed_color = RSpec.configuration.rspec_hue_failed_color
+		passed_color = RSpec.configuration.rspec_hue_passed_color
+		hue_ip = RSpec.configuration.rspec_hue_ip
+		options[:failed_color] = failed_color unless failed_color.nil?
+		options[:passed_color] = passed_color unless passed_color.nil?
+		options[:hue_ip] = hue_ip unless hue_ip.nil?
+		PhilipsHueController.new options
+	end
+	def bulb_controller
+		@bulb_controller
+	end
+end	
+
+RSpecHue.configure
---
+++ spec/rspec_hue_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe RSpecHue do
+	context "when configuring rspec settings" do
+		def set_rspec_setting setting
+			RSpec.configure do |config|
+				config.send (setting.to_s + "=").to_sym, "foo_value"
+			end
+		end
+		it "should be able to set :rspec_hue_bulb_id" do
+			expect { set_rspec_setting :rspec_hue_bulb_id }.to_not raise_exception
+		end
+		it "should be able to set :rspec_hue_ip" do
+			expect { set_rspec_setting :rspec_hue_ip }.to_not raise_exception
+		end
+		it "should be able to set :rspec_hue_failed_color" do
+			expect { set_rspec_setting :rspec_hue_failed_color }.to_not raise_exception
+		end
+		it "should be able to set :rspec_hue_passed_color" do
+			expect { set_rspec_setting :rspec_hue_passed_color }.to_not raise_exception
+		end
+	end
+	context "when corresponding to the formatter interface" do
+		let(:controller) do
+			c = double("controller")
+			c.stub(:failed)
+			c.stub(:passed)
+			c
+		end
+		let(:formatter) { RSpecHue.new StringIO.new, controller: controller }
+		it "should respond dump_summary" do
+			expect { formatter.dump_summary 123,2,1,1 }.to_not raise_exception
+		end
+	end
+end
