68b5daad54d8955cc28cedb974be5459c8b5d96f
Lars Krantz <lars.krantz@alaz.se>
2013-05-29 09:25:55 +0200

    Added integration specs when Hue is not found. They are skipped during normal rspec, see spec_helper

--- spec/philips_hue_controller_spec.rb
+++ spec/philips_hue_controller_spec.rb
@@ -18,4 +18,25 @@ describe PhilipsHueController do
 			controller.passed
 		end
 	end
+	context "Integration", integration: true do
+		context "when no Hue is found" do
+			let(:output) { StringIO.new }
+			let(:controller_no_hue) { PhilipsHueController.new ssdp_port: 2000, bulb_id_to_use: 1, output: output }
+
+			it "should not raise an error on initialization" do
+				expect { controller_no_hue }.to_not raise_error
+			end
+			it "should not raise an error on failed" do
+				expect { controller_no_hue.failed }.to_not raise_error
+			end
+			it "should not raise an error on passed" do
+				expect { controller_no_hue.passed }.to_not raise_error
+			end
+
+			it "should output an error text starting with RspecHue" do
+				controller_no_hue
+				output.string.should start_with "RspecHue:"
+			end
+		end
+	end
 end
